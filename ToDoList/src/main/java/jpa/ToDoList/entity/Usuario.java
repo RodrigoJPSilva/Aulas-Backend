ed0f954ae15ca5b5d5f796416ad7c5d6c9ad0145
RodrigoJPSilva <rodrigojrpsilva@gmail.com>
2025-09-24 09:55:31 -0300

    Fix: added some changes

--- ToDoList/src/main/java/jpa/ToDoList/entity/Usuario.java
+++ ToDoList/src/main/java/jpa/ToDoList/entity/Usuario.java
@@ -14,6 +14,7 @@ public class Usuario {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+
     private String nome;
     private String email;
     private String senha;
