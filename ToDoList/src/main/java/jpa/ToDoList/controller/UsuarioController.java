5409173105967bccbfd2c4b8ad0e434b301c17f2
RodrigoJPSilva <rodrigojrpsilva@gmail.com>
2025-10-30 11:17:08 -0300

    Feat: added auth-pages tests

--- ToDoList/src/main/java/jpa/ToDoList/controller/UsuarioController.java
+++ ToDoList/src/main/java/jpa/ToDoList/controller/UsuarioController.java
@@ -1,29 +1,29 @@
-package jpa.ToDoList.controller;
-
-import jpa.ToDoList.entity.Usuario;
-import jpa.ToDoList.repositories.UsuarioRepositories;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
-
-@RestController
-public class UsuarioController{
-
-    @Autowired
-    UsuarioRepositories user;
-
-//    @PostMapping
-//    Public Usuario Salvar() {
-//        Usuario teste = new Usuario("teste", "teste@gmail.com", "lkdjsf");
-//        return user.save(teste);
+//package jpa.ToDoList.controller;
 //
-    @GetMapping
-    public List<Usuario> mostrar() {
-        List<Usuario> usuario = user.findAll();
-        return usuario;
-    }
-}
+//import jpa.ToDoList.entity.Usuario;
+//import jpa.ToDoList.repositories.UsuarioRepositories;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.PostMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//import java.util.List;
+//
+//@RestController
+//public class UsuarioController{
+////
+////    @Autowired
+////    UsuarioRepositories user;
+//
+////    @PostMapping
+////    Public Usuario Salvar() {
+////        Usuario teste = new Usuario("teste", "teste@gmail.com", "lkdjsf");
+////        return user.save(teste);
+////
+//    @GetMapping
+//    public List<Usuario> mostrar() {
+//        List<Usuario> usuario = user.findAll();
+//        return usuario;
+//    }
+//}
---
+++ auth_pages-main/login/src/main/java/com/login/login/controller/ProdutoController.java
@@ -0,0 +1,65 @@
+package com.login.login.controller;
+
+import com.login.login.entity.Produto;
+import com.login.login.repository.ProdutoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/produto")
+public class ProdutoController {
+
+    @Autowired
+    ProdutoRepository produtoRepository;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> buscarId(@PathVariable int id) {
+
+        Optional<Produto> produto = produtoRepository.findById(id);
+
+        if (produto.isPresent()) {
+            return ResponseEntity.ok(produto);
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Não encontrado");
+        }
+    }
+
+    @GetMapping("/all")
+    public List<Produto> buscarTodosId() {
+        return produtoRepository.findAll();
+    }
+    @PutMapping("/{id}")
+    public ResponseEntity<Produto> atualizar (@PathVariable int id, @RequestBody Produto novoProduto) {
+        Optional<Produto> produtoExistente = produtoRepository.findById(id);
+
+        if (produtoExistente.isPresent()) {
+            Produto produto = produtoExistente.get();
+            produto.setQuantity(novoProduto.getQuantity());
+            produtoRepository.save(produto);
+            return ResponseEntity.ok(produto);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<String> deletarPorId(@PathVariable int id) {
+        if (produtoRepository.existsById(id)) {
+            produtoRepository.deleteById(id);
+            return ResponseEntity.status(HttpStatus.OK).body("Excluido com sucesso!");
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Não encontrado");
+        }
+    }
+    @PostMapping(value = "/cadastro")
+    public ResponseEntity<?> saveProduct(@RequestBody Produto product) {
+        Produto produto = new Produto(product.getName(), product.getPrice(), product.getQuantity());
+        produtoRepository.save(product);
+        return ResponseEntity.ok("O produto foi cadastrado com sucesso!");
+    }
+}
+
